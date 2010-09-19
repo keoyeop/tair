a97cc92e9a74e3c185f4550c324c90a095b809dc
ruohai@taobao.com <ruohai@taobao.com@ed5bfd34-b61d-4ad5-b956-f82ce90ca771>
2010-09-19 05:18:09 +0000

    change copyright information
    git-svn-id: http://code.taobao.org/svn/tair/trunk@28 ed5bfd34-b61d-4ad5-b956-f82ce90ca771

--- src/packets/migrate_finish_packet.hpp
+++ src/packets/migrate_finish_packet.hpp
@@ -1,5 +1,5 @@
 /*
- * (C) 2007-2010 Taobao Inc.
+ * (C) 2007-2010 Alibaba Group Holding Limited
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
