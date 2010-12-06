cdd9393ddfb090d599bf80a0b447fb577a45c010
huzhenxiong.pt@taobao.com <huzhenxiong.pt@taobao.com@ed5bfd34-b61d-4ad5-b956-f82ce90ca771>
2010-12-06 07:51:52 +0000

    add test data manager in test_kdb_base.h
    git-svn-id: http://code.taobao.org/svn/tair/branches/dev@59 ed5bfd34-b61d-4ad5-b956-f82ce90ca771

--- test/interface_test/test_kdb_base.h
+++ test/interface_test/test_kdb_base.h
@@ -2,6 +2,8 @@
 #include <tbsys.h>
 #include "data_entry.hpp"
 
+#define TEST_DATA_COUNT 100
+
 using namespace tair::common; 
 
 class TestKdbBase: public ::testing::Test
@@ -37,6 +39,14 @@ class TestKdbBase: public ::testing::Test
       if (compareDataValue(v1, v2) == false) return false;
       return memcmp(&v1.data_meta, &v2.data_meta, sizeof(v1.data_meta));
     }
+    
+    static void print(data_entry &data) 
+    {
+      char * temp = data.get_data();
+      int i = (int)temp[0] + ((int)temp[1]) * 10;
+      printf("area:%d\tdata:%s\n", i, temp+2);
+    }
+
 
   protected:
 };
@@ -44,9 +54,21 @@ class TestKdbBase: public ::testing::Test
 class TestKdbData
 {
   public:
+    TestKdbData()
+    {
+      set_test_data();
+    }
+
     void set_test_data()
     {
-      
+      char data1[] = "hello_kdb_key";
+      char data2[] = "hello_kdb_value";
+      for(int i = 0; i < TEST_DATA_COUNT; i++) {
+        keys[i].set_data(data1, strlen(data1),true);
+        keys[i].merge_area(i);
+        values[i].set_data(data2, strlen(data2), true);
+        values[i].merge_area(i);
+      }
     }
 
     data_entry * get_test_key(int index)
@@ -60,7 +82,7 @@ class TestKdbData
     }
  
  private:
-    data_entry keys[100];
-    data_entry values[100];
+    data_entry keys[TEST_DATA_COUNT];
+    data_entry values[TEST_DATA_COUNT];
 };
 
